0be92ef8a4dc6ba62cf5315f90fad5aa9cc36c28
TheC0dingBear <cdusseldorp@live.nl>
2022-04-29 09:50:59 +0200

    name updates

--- resources/views/components/card-single-accommodation.blade.php
+++ resources/views/components/card-single-accommodation.blade.php
@@ -1,5 +1,6 @@
 <div class="c-card__single">
     <p class="font-bold">
+<<<<<<< Updated upstream
         {{ $accommodation->name }}
     </p>
     <p class="swiper-no-overflow mt-4">
@@ -7,6 +8,15 @@
     </p>
     <p class="my-4">
         €{{ $accommodation->price }}
+=======
+        {{ $accommodation->title }}
+    </p>
+    <p class="swiper-no-overflow mt-4">
+        {{ $accommodation->shortDescription }}
+    </p>
+    <p class="my-4">
+        <img src="{{ $accommodation->imageUrl }}">
+>>>>>>> Stashed changes
     </p>
     <a href="{{ '/accommodation/'.$accommodation->id }}" class="c-button c-button__default" href="#">
         Meer informatie
